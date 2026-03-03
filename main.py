c4a8dbc9c9ca787ea293b5b090b019142a6cfb4c
AJ-18 <arjun.suresh1@marist.edu>
2026-03-03 10:59:21 -0500

    Implementing Lines per page

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
          ln=1)
-    pdf.line(10, 20, 200, 20)
+
+    # Generate lines
+    for y in range(20, 281, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(260)
@@ -33,5 +36,9 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Generate lines
+        for y in range(20, 281, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
